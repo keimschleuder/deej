92a15cc7cdf1da962341872921a5bf76ae07f9b1
Niklas <138971666+keimschleuder@users.noreply.github.com>
2024-05-11 21:52:17 +0200

    Overengineering the arduino stuff

--- arduino/deej/deej.ino
+++ arduino/deej/deej.ino
@@ -1,12 +1,11 @@
 const int NUM_SLIDERS = 6;
-const int analogInputs[NUM_SLIDERS] = {A0, A1, A3, A6, A7, A8};
+const int analogInputs[NUM_SLIDERS] = { A0, A1, A3, A6, A7, A8 };
 const int NUM_BUTTONS = 6;
-const int buttonInputs[NUM_BUTTONS] = {0, 1, 2, 3, 5, 7};
+const int buttonInputs[NUM_BUTTONS] = { 0, 1, 2, 3, 5, 7 };
 const float noiseReduction = 0.02
 
-int analogSliderValues[NUM_SLIDERS];
 float percentSliderValues[NUM_SLIDERS];
-int lastSliderValues[NUM_SLIDERS];
+float lastSliderValues[NUM_SLIDERS];
 int buttonValues[NUM_BUTTONS];
 
 void setup() {
@@ -25,22 +24,24 @@ void loop() {
   updateSliderValues();
   sendSliderValues(); // Actually send data (all the time)
 //   printSliderValues(); // For debug
+  lastSliderValues = percentSliderValues;
   delay(10);
-  lastSliderValues = analogSliderValues;
 }
 
 void updateSliderValues() {
   // Sliders
   percentSliverValues = [];
   for (int i = 0; i < NUM_SLIDERS; i++) {
-    analogSliderValues[i] = analogRead(analogInputs[i]);
+    int analogValue = analogRead(analogInputs[i]);
 
     // Convert to percentages + Noise cancelling
-    float dirtyFloat = analogSliderValues[i] / 1023.0;
-    float normalized = normalizeValue(dirtyFloat); // Two decimal digits
+    float dirtyFloat = analogValue / 1023.0;
+    float normalized = normalizeValue(dirtyFloat);  // Two decimal digits
 
     if (hasChanged(normalized, lastSliderValues[i])) {
       percentSliderValues[i] = normalized;
+    } else {
+      percentSliderValues[i] = 0;
     }
   }
   // Screen updating
@@ -54,16 +55,26 @@ void updateSliderValues() {
 void sendSliderValues() {
   String builtString = String("");
 
+  /* slightly overengineering this stuff. */
+  bool sliderChanged = false;
   for (int i = 0; i < NUM_SLIDERS; i++) {
     builtString += "s";
-    builtString += String((int)analogSliderValues[i]);
+    if (percentSliderValues[i] != 0) {
+      if (sliderChanged) {
+        builtString += "|";
+      }
+      sliderChanged = true;
 
-    if (i < NUM_SLIDERS - 1) {
-      builtString += String("|");
+      builtString += String((int)i);
+      builtString += "x";
+      builtString += String((float)percentSliderValues[i]);
     }
   }
+  if (!sliderChanged) {
+    builtString += "-1x-1";
+  }
 
-  if(NUM_BUTTONS > 0){
+  if (NUM_BUTTONS > 0) {
     builtString += String("|");
   }
 
@@ -92,11 +103,11 @@ bool hasChanged(int new, int old) {
 }
 
 bool almostEqual(float a, float b) {
-  return abs(a - b) > 0.00001
+  return abs(a - b) > 0.01
 }
 
 float normalizeValue(float v) {
-  float result = floor(v * 100) / 100.0;
+  float result = floor(v * 100) / 100.0;  // Auf 2 Nachkommastellen kürzen: 0.99856 -> 0.99
   return result;
 }
 
