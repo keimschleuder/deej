9ce07b616249440ea908435682772eacd736e7bb
Niklas <138971666+keimschleuder@users.noreply.github.com>
2024-05-14 21:07:57 +0200

    Update deej.ino

--- arduino/deej/deej.ino
+++ arduino/deej/deej.ino
@@ -6,7 +6,7 @@ const float noiseReduction = 0.02
 
 float percentSliderValues[NUM_SLIDERS];
 float lastSliderValues[NUM_SLIDERS];
-int buttonValues[NUM_BUTTONS];
+bool buttonValues[NUM_BUTTONS];
 
 void setup() {
   for (int i = 0; i < NUM_SLIDERS; i++) {
@@ -48,7 +48,7 @@ void updateSliderValues() {
 
   // Buttons
   for (int i = 0; i < NUM_BUTTONS; i++) {
-    buttonValues[i] = digitalRead(buttonInputs[i]);
+    buttonValues[i] = (digitalRead(buttonInputs[i]) == HIGH);
   }
 }
 
@@ -56,36 +56,49 @@ void sendSliderValues() {
   String builtString = String("");
 
   /* slightly overengineering this stuff. */
-  bool sliderChanged = false;
+  bool changed = false;
   for (int i = 0; i < NUM_SLIDERS; i++) {
     if (percentSliderValues[i] != 0) {
-      if (sliderChanged) {
+      if (changed) {
         builtString += "|";
+      } else {
+        changed = true;
       }
-      sliderChanged = true;
 
       builtString += "s";
       builtString += String((int)i);
       builtString += "x";
       builtString += String((float)percentSliderValues[i]);
+      /*Format: "s{Slider Index}x{Slider Vaule (0.0 - 1.0 as %)}*/
     }
   }
-  if (!sliderChanged) {
-    builtString += "-1x-1";
+  if (!changed) {
+    builtString += "s-1x-1";
   }
 
   if (NUM_BUTTONS > 0) {
     builtString += String("|");
   }
 
+  changed = false
   for (int i = 0; i < NUM_BUTTONS; i++) {
-    builtString += "b";
-    builtString += String((int)buttonValues[i]);
-
-    if (i < NUM_BUTTONS - 1) {
-      builtString += String("|");
+    if (buttonValues[i]) {
+      if (changed) {
+        builtString += "|";
+      } else {
+        changed = true;
+      }
+      builtString += "b";
+      builtString += String((int)i);
+      /*Returns every pressedes Button as: b{index}*/
     }
   }
+  if (!changed) {
+    builtString += "b-1";
+  }
+
+  /*Default is: s-1x-1|b-1
+  Whenever a value chnages or a button is pressed the builtString changed as documented above*/
 
   Serial.println(builtString);
 }
