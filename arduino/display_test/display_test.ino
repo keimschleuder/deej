ce992f9a6b945e7a6182a81a6399c14648cc4a23
Niklas <138971666+keimschleuder@users.noreply.github.com>
2025-12-31 20:20:04 +0100

    Update the screen percentage code

--- arduino/display_test/display_test.ino
+++ arduino/display_test/display_test.ino
@@ -6,6 +6,8 @@
 #define dc   2
 #define rst  3
 
+const char* sliderNames[] = {"Master Volume", "Aktuelles Fenter", "Discord", "Musik", "Mikrofon"};
+
 TFT TFTscreen = TFT(cs, dc, rst);
 uint8_t red = 255;
 uint8_t green = 255;
@@ -20,7 +22,7 @@ void setup() {
   TFTscreen.stroke(blue, green, red);
 }
 
-void displayPercentage(uint8_t percentage) {
+void displayPercentage(uint8_t percentage, uint8_t slider) {
   // Textfarbe weiss
   TFTscreen.stroke(255, 255, 255);
   // Rechteck-Rahmen
@@ -33,23 +35,31 @@ void displayPercentage(uint8_t percentage) {
   // Alte Zahl löschen (schwarzes Rechteck über den Bereich)
   TFTscreen.fill(0, 0, 0);
   TFTscreen.noStroke();
-  TFTscreen.rect(20, 45, 78, 35);
+  TFTscreen.rect(20, 40, 110, 45);
   
   // Neue Zahl schreiben
   TFTscreen.stroke(255, 255, 255);
   char buffer[4];
   sprintf(buffer, "%02d", percentage);
-  TFTscreen.text(buffer, 30, 50);
-  TFTscreen.text("%", 102, 50);
+  TFTscreen.text(buffer, 30, 55);
+  TFTscreen.text("%", 102, 55);
+
+  // Label schreiben
+  TFTscreen.textSize(1);
+  if(slider < 5) {
+    TFTscreen.text(sliderNames[slider], 30, 40);
+  }
 }
 
 void loop() {
-  displayPercentage(10);
+  displayPercentage(10, 0);
   delay(1000);
-  displayPercentage(0);
+  displayPercentage(0, 1);
   delay(1000);
-  displayPercentage(100);
+  displayPercentage(100, 2);
   delay(1000);
-  displayPercentage(50);
+  displayPercentage(50, 3);
+  delay(1000);
+  displayPercentage(25, 4);
   delay(1000);
 }
